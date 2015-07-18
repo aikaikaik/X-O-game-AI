cf1891d2f971e4e307fc09c54426ed397d09df50
arad keren <aradk1@gmail.com>
2015-07-18 18:33:35 +0300

    whoWon

--- main.js
+++ main.js
@@ -6,7 +6,33 @@ var Board = function(){
   ];
   return {
     getData: function() {return boardData;},
-    addSq: function(obj) {var sqVal = boardData[obj.x][obj.y]; if(typeof sqVal !== 'string' && sqVal === null && (obj.val==='X' || obj.val==='O')){boardData[obj.x][obj.y]=obj.val}}
+    addSq: function(obj) {var sqVal = boardData[obj.x][obj.y]; if(typeof sqVal !== 'string' && sqVal === null && (obj.val==='X' || obj.val==='O')){boardData[obj.x][obj.y]=obj.val}},
+    whoWon: function() {
+      var didWon = function(player){
+        var playerRow = [player,player,player];
+        //rows
+        for(var row = 0; row < 3; row++){
+          if(boardData[row][0]===player&&boardData[row][1]===player&&boardData[row][2]===player){return true;}
+        }
+        //columns
+        for(var colNum = 0; colNum < 3; colNum++){
+          if(boardData[0][colNum]===player&&boardData[1][colNum]===player&&boardData[2][colNum]===player){return true;}
+        }
+        ///
+        if(boardData[0][0]===player&&boardData[1][1]===player&&boardData[2][2]===player){return true;}
+        if(boardData[0][2]===player&&boardData[1][1]===player&&boardData[2][0]===player){return true;}
+        //else...
+        return false;
+      }
+      if(didWon('X')){return 'X';}
+      if(didWon('O')){return 'O';}
+      for(var row = 0; row<3; row++){
+        for(var sq = 0; sq<3; sq++){
+          if(boardData[row][sq]===null){return false;}
+        }
+      }
+      return '=';
+    }
   };
 };
 var fakeBoard = function(board,player){
@@ -43,10 +69,12 @@ var game = function(player1,player2){
     console.change(change);
     board.addSq(change);
     console.board(board);
+    console.log(board.whoWon());
   }
   var board = Board();
   console.board(board);
-  turn(player1,'O');
-  turn(player2,'X');
-  turn(player1,'O');
+  for(var i = 0; i<10; i++){
+    turn(player1,'O');
+    turn(player2,'X');
+  }
 };
