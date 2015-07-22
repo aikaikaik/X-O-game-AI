57149e45e8e9f63a2b46d7abd408d3e78bcc88e1
arad keren <aradk1@gmail.com>
2015-07-22 19:13:50 +0300

    ask user which player programs he wishes to use

--- mainScript.js
+++ mainScript.js
@@ -1,8 +1,20 @@
 //run with node.js
 require('./niceLog.js');
 var game = require('./game.js');
+//readline
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 //players
-var p1 = require('./player-random');
-var p2 = require('./player-random');
-
-game(p1,p2);
+var p1;
+var p2;
+rl.question('p1 is on (e.g. "./player-random.js")?: ',function(strp1){
+  p1 = require(strp1);
+  rl.question('p2 in on(same)?: ',function(strp2){
+    p2 = require(strp2);
+    game(p1,p2);
+    rl.close();
+  });
+});
