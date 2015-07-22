022a97e0e7d0babb811f9c7978d3315b9c3c1e26
arad keren <aradk1@gmail.com>
2015-07-22 19:00:33 +0300

    only legal

--- player-random.js
+++ player-random.js
@@ -1,2 +1,10 @@
-var player_random = function(){return {'x':Math.floor(Math.random()*3),'y':Math.floor(Math.random()*3)}};
+var player_random = function(board){
+  var data = board.getData();
+  var simple_random = function(){
+    return {'x':Math.floor(Math.random()*3),'y':Math.floor(Math.random()*3)};
+  };
+  var result = simple_random();
+  console.change(result);
+  if(data[result.x][result.y]===null){return result;}else{return player_random(board);}
+};
 module.exports = player_random;
