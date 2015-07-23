c80d14d5743d2e4943005d995b5605d937f59bf5
arad keren <aradk1@gmail.com>
2015-07-23 09:24:05 +0300

    networks

--- mainScript.js
+++ mainScript.js
@@ -1,6 +1,8 @@
 //run with node.js
 require('./niceLog.js');
 var game = require('./game.js');
+var fs = require('fs');
+var fromNetToFunc = require('./runNet.js');
 //readline
 var readline = require('readline');
 var rl = readline.createInterface({
@@ -11,9 +13,9 @@ var rl = readline.createInterface({
 var p1;
 var p2;
 rl.question('p1 is on (random, or firstOpen)?: ',function(strp1){
-  p1 = require('./players/'+strp1+'.js');
+  p1 = fromNetToFunc(JSON.parse(fs.readFileSync('./nets/'+strp1+'.net.json')));
   rl.question('p2 in on(same)?: ',function(strp2){
-    p2 = require('./players/'+strp2+'.js');
+    p2 = fromNetToFunc(JSON.parse(fs.readFileSync('./nets/'+strp2+'.net.json')));
     //how meny times?
     rl.question('how many games do you want?: ',function(gameNumber){
       //set up statistics numbers
@@ -26,8 +28,8 @@ rl.question('p1 is on (random, or firstOpen)?: ',function(strp1){
           gameEndBoards.push(game(p1,p2,(!toRepBool || i/2===Math.floor(i/2))?true:false));
           whoWonStats[gameEndBoards[i].whoWon()]++;
         }
-        console.log('X(p1) won '+whoWonStats.X/gameNumber*100+'%');
-        console.log('O(p2) won '+whoWonStats.O/gameNumber*100+'%');
+        console.log('X('+strp1+') won '+whoWonStats.X/gameNumber*100+'%');
+        console.log('O('+strp2+') won '+whoWonStats.O/gameNumber*100+'%');
         console.log('\'twas a match '+whoWonStats['=']/gameNumber*100+'%');
         rl.close();
       });
---
+++ runNet.js
@@ -0,0 +1,45 @@
+var getNodeVals = function(board,network){
+  var nodeVals = [];
+  var sum = 0;
+  var data = board.getData();
+  for(var node in network.nodes){
+    nodeVals.push(0);
+  }
+  for(var edgeNum = 0; edgeNum< network.edges.length; edgeNum++){
+    var edge  = network.edges[edgeNum];
+    if(data[edge.lightOn.x][edge.lightOn.y]===edge.lightOn.val){
+      nodeVals[edge.lightNodeNum]++;
+      sum++;
+    }
+  }
+  for(var nodeNum = 0; nodeNum < network.nodes.length; nodeNum++){
+    nodeVals[nodeNum]=nodeVals[nodeNum]/sum;
+  }
+  return nodeVals;
+};
+
+var getProbBoard = function(network,nodeVals){
+  var probBoard = [
+    [0,0,0]
+    ,[0,0,0]
+    ,[0,0,0]
+  ];
+  for(var i = 0; i<9; i++){
+    for(var nodeNum = 0; nodeNum < network.nodes.length; nodeNum++){
+      probBoard[Math.floor(i/3)][i%3]+=network.nodes[nodeNum][Math.floor(i/3)][i%3]*nodeVals[nodeNum];
+    }
+  }
+  return probBoard;
+}
+
+var getRandomSq = function(board,boardWeights) {
+  var result = {'x':Math.floor(Math.random()*3),'y':Math.floor(Math.random()*3)};
+  var random = Math.random();
+  if(random<boardWeights[result.x][result.y] && board.getData()[result.x][result.y]===null){return result;}else{return getRandomSq(board,boardWeights);}
+}
+
+module.exports = function(network){
+  return function(board){
+    return getRandomSq(board,getProbBoard(network,getNodeVals(board,network)));
+  };
+};
