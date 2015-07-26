076ccd99c85c21552d42ba330070441b4dfb5cd4
arad keren <aradk1@gmail.com>
2015-07-26 12:55:21 +0300

    update data on server every half a minute

--- display_runNet_server.js
+++ display_runNet_server.js
@@ -1,24 +1,29 @@
 var http = require('http');
 var fs = require('fs');
-fs.readFile('./netData.json',function(err,netDataStr){
-  if(err) throw err;
+var tableBody = '';
+var getData = function(){
+  var netDataStr = fs.readFileSync('./netData.json');
   console.log(netDataStr);
-  var tableBody = '';
   var data = JSON.parse(netDataStr);
+  var _tableBody = '';
   for(var a in data){
-    tableBody+='<tr><th>'+a+'</th>';
+    _tableBody+='<tr><th>'+a+'</th>';
     var sum = 0;
     var n = 0;
     for(var b in data[a]){
-      tableBody+='<td'+((a===b)?' class="b"':'')+'>'+Math.round(data[a][b].me/data[a][b].gameNumber*100)+'%</td>';
+      _tableBody+='<td'+((a===b)?' class="b"':'')+'>'+Math.round(data[a][b].me/data[a][b].gameNumber*100)+'%</td>';
       sum += data[a][b].me/data[a][b].gameNumber*100;
       n++;
     }
-    tableBody += '<td class="b">'+Math.round(sum/n*10)/10+'%</td>'
-    tableBody+='</tr>';
+    _tableBody += '<td class="b">'+Math.round(sum/n*10)/10+'%</td>'
+    _tableBody+='</tr>';
   }
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<!doctype html><html><head><title>net data</title><style>.b{background-color:#333;color:#eee;}</style></head><body><h1>net data</h1><table>'+tableBody+'</table></body></html>');
-  }).listen(1337, '127.0.0.1');
-});
+  _tableBody+='</table>';
+  tableBody = _tableBody;
+};
+getData();
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<!doctype html><html><head><title>net data</title><style>.b{background-color:#333;color:#eee;}</style></head><body><h1>net data</h1><table>'+tableBody+'</table></body></html>');
+}).listen(1337, '127.0.0.1');
+var a = setInterval(getData, 30000);
