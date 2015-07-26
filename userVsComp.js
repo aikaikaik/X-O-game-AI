b76da76ec635ade0ef196a043f837f40d5296955
arad keren <aradk1@gmail.com>
2015-07-26 12:57:56 +0300

    now you can use userVsComp with a parameter

--- userVsComp.js
+++ userVsComp.js
@@ -1,12 +1,22 @@
 require('./niceLog.js');
 var game = require('./game.js');
+
 var fs = require('fs');
+
 var fromNetToFunc = require('./runNet.js');
+
 //readline
 var rls = require('readline-sync');
-var pnameToFunc = function(strp){return fromNetToFunc(JSON.parse(fs.readFileSync('./nets/'+strp+'.net.json')));};
-var net = pnameToFunc(rls.question('What net?: '));
+var pfileNameToFunc = function(strp){return fromNetToFunc(JSON.parse(fs.readFileSync(strp)));};
+
+var net = (process.argv.length>2)?pfileNameToFunc(process.argv[2]):pfileNameToFunc('./nets/'+rls.question('What net?: ')+'.net.json');
+
 var user = function(b){
+
+  console.board(b);
+
   return {'x':rls.question('x: '),'y':rls.question('y: ')};
+
 };
+
 game(net,user,true);
