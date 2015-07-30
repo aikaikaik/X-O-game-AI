be026eaf4864841c3f312bba473ecdbc6108d234
arad keren <aradk1@gmail.com>
2015-07-30 15:17:06 +0300

    mutate & run net

--- nets.js
+++ nets.js
@@ -3,11 +3,26 @@
 var Perceptron = [];
 // input layer -> output layer
 Perceptron.run = function(inputLayer){
-
+  var sigmoid = function(x){return 1/(1+Math.pow(Math.E,-x));};
+  var outputLen = this[0].length;
+  var inputLen = this.length;
+  var result = [];
+  console.assert(this.length===inputLayer.length,'bad data: this.length: '+this.length+'while inputLayer.length: '+inputLayer.length);
+  for(var i = 0; i < outputLen; i++){
+    var sum = this.reduce(function(pval,val,j,arr){return pval+val[i]*inputLayer[i];},0);
+    result[i] = sigmoid(sum);
+  }
+  return result;
 };
 // mutationRate -> mutated net
-Perceptron.mutate = function(mutationRate){
-
+Perceptron.mutate = function(mutationRate,mutationFunc){
+  var result = this;
+  for(var i = 0; i < this.length; i++){
+    for(var j = 0; j < this[i].length; j++){
+      if(Math.random()<mutationRate){result[i][j] = mutationFunc(this[i][j]);}
+    }
+  }
+  return result;
 };
 var makePerceptron = function(lena,lenb,makeFunc){
   var perceptron = [];
@@ -27,11 +42,20 @@ var makePerceptron = function(lena,lenb,makeFunc){
 var Net = [];
 // input layer -> output layer
 Net.run = function(inputLayer){
-
+  var currentLayer = inputLayer;
+  for(var i = 0; i < this.length; i++){
+    currentLayer.push(-1);
+    currentLayer = this[i].run(currentLayer);
+  }
+  return currentLayer;
 };
 // mutationRate -> mutated net
-Net.mutate = function(mutationRate){
-
+Net.mutate = function(mutationRate,mutationFunc){
+  var result = this;
+  for(var i = 0; i < this.length; i++){
+    result[i] = this[i].mutate(mutationRate,mutationFunc);
+  }
+  return result;
 };
 module.exports.makeNet = function(layerLens,makeFunc){
   var net = [];
