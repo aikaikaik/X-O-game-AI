1808032ff0754291eb6627a3c3d3f429e9595ba1
arad keren <aradk1@gmail.com>
2015-08-01 17:08:18 +0300

    save every X gens

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ var mutationRate = 0.2;
 var hiddenLayerLen = 18;
 var poolLen = 6;
 var genNumber = rls.question('How many gens?: ');
+var id = Math.floor(Math.random()*1000);
+var saveEvery = 12^4;
 
 //setup
 var net = nets.makeNet([18,hiddenLayerLen,9],function(){return Math.floor(Math.random()*10)/10;});
 var pool = [];
 for(var i = 0;  i < poolLen; i++){pool.push(net);}
 
-//gens
 var gen = function(){
   var randomIndex = function(other){var a = Math.floor(Math.random()*pool.length);if(a===other){return randomIndex(other);}else{return a;}};
   var p1index = randomIndex(NaN);
@@ -26,16 +27,25 @@ var gen = function(){
   var worsePindex = (betterPindex===p1index)?p2index:p1index;
   pool[worsePindex] = pool[betterPindex].mutate(mutationRate,function(val){return val+Math.floor((Math.random()*0.75-0.375)*100)/100;});
 };
-for(var i = 0; i < genNumber; i++){
-  if(i%12===0){console.log('beginning generation number '+i);}
-  gen();
-}
-console.log('end of gens');
 
-//end
-var dirName = './pool'+Math.floor(Math.random()*1000);
-fs.mkdirSync(dirName);
-for(var i = 0; i < pool.length; i++){
-  fs.writeFileSync(dirName+'/'+i+'.net.json',JSON.stringify(pool[i]));
-}
-console.log('end');
+var main = function(){
+  for(var i = 0; i < genNumber; i++){
+    if(i%12===0){console.log('beginning generation number '+i);}
+    gen();
+    if(i%10000===0){console.log('file Save');saveToFile(i);}
+  }
+  console.log('end of gens');
+  saveToFile(i);
+  console.log('end');
+};
+
+var saveToFile = function(i){
+  var dirName = './run'+id+'/gen'+i;
+  fs.mkdirSync(dirName);
+  for(var i = 0; i < pool.length; i++){
+    fs.writeFileSync(dirName+'/'+i+'.net.json',JSON.stringify(pool[i]));
+  }
+};
+
+//run main
+main();
