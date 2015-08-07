15bc6dfeaa657c5cf874f060d2c6d63f293288e7
arad keren <aradk1@gmail.com>
2015-08-07 07:56:02 +0300

    user vs comp, now with file from parameter option

--- userVsComp.js
+++ userVsComp.js
@@ -4,7 +4,7 @@ var rls = require('readline-sync');
 var netToPlayer = require('./netXOplayer.js');
 var nets = require('./nets.js');
 var fs = require('fs');
-
-var compP = netToPlayer(nets.netFromJSON(fs.readFileSync(rls.question('computer player?: '))));
+var compPname = process.argv[2] || rls.question('computer player?: ');
+var compP = netToPlayer(nets.netFromJSON(fs.readFileSync(compPname)));
 
 game(compP,function(board){console.board(board);return {'x':rls.question('x: '),'y':rls.question('y: ')};},true,true);
